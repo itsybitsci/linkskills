0d57efb4ed1e470181023c5eb392030ba706ff3e
James Marnie Argarin <jnargarin@gmail.com>
2023-09-13 17:31:51 +0800

    feat: Added project base files

---
+++ components/home/welcome/Welcome.jsx
@@ -0,0 +1,21 @@
+import { useState} from 'react'
+import { View, Text, TextInput, TouchableOpacity, Image, FLatlist } from 'react-native'
+import { useRouter } from 'expo-router'
+
+import styles from './welcome.style'
+import {icons, SIZES} from '../../../constants'
+
+const Welcome = () => {
+	const router = useRouter()
+
+  return (
+    <View>
+      <View style={styles.container}>
+				<Text style={styles.userName}>Hello itsybitsci</Text>
+				<Text style={styles.welcomeMessage}>Find your perfect job</Text>
+			</View>
+    </View>
+  )
+}
+
+export default Welcome
