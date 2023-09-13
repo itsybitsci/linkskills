37088fbd62752199b33eb1d699d7a1bf18e3c068
James Marnie Argarin <jnargarin@gmail.com>
2023-09-13 18:24:20 +0800

    feat: Added async fonts loading and welcome UI

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,12 +1,15 @@
-import { useState} from 'react'
-import { View, Text, TextInput, TouchableOpacity, Image, FLatlist } from 'react-native'
-import { useRouter } from 'expo-router'
+import { useState} from 'react';
+import { View, Text, TextInput, TouchableOpacity, Image, FlatList } from 'react-native';
+import { useRouter } from 'expo-router';
 
-import styles from './welcome.style'
-import {icons, SIZES} from '../../../constants'
+import styles from './welcome.style';
+import {icons, SIZES} from '../../../constants';
+
+const jobTypes = ['Full-time', 'Part-time', 'Internship', 'Contractor'];
 
 const Welcome = () => {
-	const router = useRouter()
+	const router = useRouter();
+	const [activeJobType, setActiveJobType] = useState(jobTypes[0]);
 
   return (
     <View>
@@ -14,8 +17,47 @@ const Welcome = () => {
 				<Text style={styles.userName}>Hello itsybitsci</Text>
 				<Text style={styles.welcomeMessage}>Find your perfect job</Text>
 			</View>
+
+			<View style={styles.searchContainer}>
+				<View style={styles.searchWrapper}>
+					<TextInput
+						style={styles.searchInput}
+						value=''
+						onChange={() => {}}
+						placeholder='Search for jobs'
+					/>
+				</View>
+
+				<TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+					<Image 
+						source={icons.search}
+						resizeMode='contain'
+						style={styles.searchBtnImage}
+					/>
+				</TouchableOpacity>
+			</View>
+
+			<View style={styles.tabsContainer}>
+				<FlatList
+					data={jobTypes}
+					renderItem={({item}) => (
+						<TouchableOpacity
+							style={styles.tab(activeJobType, item)}
+							onPress={() => {
+								setActiveJobType(item);
+								router.push(`/search/${item}`);
+							}}
+						>
+							<Text style={styles.tabText(activeJobType, item)}>{item}</Text>
+						</TouchableOpacity>	
+					)}
+					keyExtractor={(item) => item}
+					contentContainerStyle={{ columnGap: SIZES.small}}
+					horizontal
+				/>
+			</View>
     </View>
-  )
+  );
 }
 
-export default Welcome
+export default Welcome;
--- components/home/welcome/welcome.style.js
+++ components/home/welcome/welcome.style.js
@@ -55,6 +55,7 @@ const styles = StyleSheet.create({
   tabsContainer: {
     width: "100%",
     marginTop: SIZES.medium,
+		alignItems: "center",
   },
   tab: (activeJobType, item) => ({
     paddingVertical: SIZES.small / 2,
