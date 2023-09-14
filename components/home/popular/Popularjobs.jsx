dab4519a61d32cc91beaf0a7cad8ed91af8fa3f2
James Marnie Argarin <jnargarin@gmail.com>
2023-09-14 18:46:38 +0800

    feat: Added popular jobs section UI

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -5,12 +5,16 @@ import { useRouter } from 'expo-router';
 import styles from "./popularjobs.style";
 import { COLORS, SIZES } from "../../../constants";
 import PopularJobCard from "../../common/cards/popular/PopularJobCard";
-// import useFetch from "../../../hook/useFetch";
+
+import useFetch from "../../../hook/useFetch";
 
 const Popularjobs = () => {
 	const router = useRouter();
-	const isLoading = false;
-	const error= false;
+	
+	const {data, isLoading, error} = useFetch('search', {
+		query: 'React Developer',
+		num_pages: 1,
+	});
 
   return (
     <View style={styles.container}>
@@ -28,10 +32,11 @@ const Popularjobs = () => {
 					<Text>Something went wrong</Text>
 				) : (
 					<FlatList 
-						data={[1,2,3,4,5,6,7,8,9,10]}
+						data={data}
 						renderItem={({item}) => (
 							<PopularJobCard 
 								item={item}
+								handleCardPress={() => {}}
 							/>
 						)}
 						keyExtractor={item => item?.job_id}
